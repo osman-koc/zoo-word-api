cb5480d6d70568e9e26c8cd555b0e5cfb6b44fc8
Osman Koç <info@osmkoc.com>
2022-04-05 23:39:40 +0300

    added express-validator package

--- api/wordApi.js
+++ api/wordApi.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const fs = require("fs");
+const { body, validationResult } = require('express-validator');
+
 const router = express.Router();
 const filePath = "./db/words.json";
 
@@ -21,53 +23,44 @@ router.get('/random', function (req, res, next) {
 });
 
 // ADD WORD
-router.post('/add', function (req, res, next) {
-    console.log("Adding word");
-    var reqBody = req.body;
-    console.log('Body: ', reqBody);
+router.post('/add',
+    body('name').isLength({ min: 2 }).withMessage('Name is too short'),
+    body('name').isLength({ max: 20 }).withMessage('Name is too long'),
+    body('relations').isLength({ min: 2 }).withMessage('Relations are too short'),
+    body('relations').isLength({ max: 20 }).withMessage('Relations are too long'),
+    function (req, res, next) {
+        console.log("Adding word");
+        console.log('Body: ', req.body);
 
-    var errorMessage = '';
-    if (reqBody === null || reqBody === undefined) {
-        errorMessage = "Body is null or missing";
-    } else if(reqBody.name === null || reqBody.name === undefined) {
-        errorMessage = "Body is missing name";
-    } else if(reqBody.relations === null || reqBody.relations === undefined) {
-        errorMessage = "Body is missing relations";
-    } else if(reqBody.name.length < 3) {
-        errorMessage = "Name is too short";
-    } else if(reqBody.relations.length < 3) {
-        errorMessage = "Relations are too short";
-    } else if(reqBody.name.length > 20) {
-        errorMessage = "Name is too long";
-    } else if(reqBody.relations.length > 20) {
-        errorMessage = "Relations are too long";
-    }
-
-    if(errorMessage !== '') {
-        console.log(errorMessage);
-        res.status(400).send(errorMessage);
-        return;
-    }
-
-    fs.readFile(filePath, 'utf8', function (err, data) {
-        if (data === null || data === undefined) {
-            console.log("No words found");
-            res.send("");
-            return;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
 
-        var dbWords = JSON.parse(data);
-        dbWords.push(reqBody);
-        fs.writeFile(filePath, JSON.stringify(dbWords), function (err) {
-            if (err) {
-                console.log("Error writing to file");
-                res.send("Error writing to file");
+        req.body.name = req.body.name.toLocaleUpperCase('tr-TR');
+        req.body.relations.forEach(function (relation, index) {
+            req.body.relations[index] = relation.toLocaleUpperCase('tr-TR');
+        });
+
+        fs.readFile(filePath, 'utf8', function (err, data) {
+            if (data === null || data === undefined) {
+                console.log("No words found");
+                res.send("");
                 return;
             }
-            console.log("Successfully wrote to file");
-            res.send("Successfully wrote to file");
+
+            var dbWords = JSON.parse(data);
+            dbWords.push(req.body);
+            fs.writeFile(filePath, JSON.stringify(dbWords), function (err) {
+                if (err) {
+                    console.log("Error writing to file");
+                    res.send("Error writing to file");
+                    return;
+                }
+                console.log("Successfully wrote to file");
+                res.send("Successfully wrote to file");
+            });
         });
     });
-});
 
 module.exports = router;
